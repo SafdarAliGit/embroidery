d57c96c61828779c23f1ef3666b5bf1e77baf643
SafdarAliGit <safdar211@gmail.com>
2025-09-26 22:52:38 +0500

    updated

--- embroidery/embroidery/doctype/gate_outward/gate_outward.js
+++ embroidery/embroidery/doctype/gate_outward/gate_outward.js
@@ -13,3 +13,36 @@ frappe.ui.form.on("Gate Outward", {
 	},
 });
 
+frappe.ui.form.on("Gate Outward Item", {
+    embroidery_type: function(frm, cdt, cdn) {
+        calculate_rate(frm, cdt, cdn);
+    },
+    stitches: function(frm, cdt, cdn) {
+        calculate_rate(frm, cdt, cdn);
+    },
+    constant: function(frm, cdt, cdn) {
+        calculate_rate(frm, cdt, cdn);
+    }
+});
+
+function calculate_rate(frm, cdt, cdn) {
+    let row = locals[cdt][cdn];
+
+        // Default to 0
+        let rate = 0;
+
+        // Apply conditions
+        if (row.embroidery_type === "Alternate & Folding") {
+            rate = (row.stitches / 1000) * row.constant;
+        } else if (row.embroidery_type === "Sheet") {
+            rate = ((row.stitches * 3) / 1000) * row.constant * 1;
+        } else {
+            rate = 0;
+        }
+
+        // Set value in child table row
+        frappe.model.set_value(cdt, cdn, "rate", rate);
+        frappe.model.set_value(cdt, cdn, "amount", row.rate * row.qty || 0);
+}
+
+
