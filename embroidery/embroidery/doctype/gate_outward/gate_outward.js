5b1c45a6127ce00aa66a8a424b80188ef4dbef7e
SafdarAliGit <safdar211@gmail.com>
2025-09-26 23:28:52 +0500

    updated

--- embroidery/embroidery/doctype/gate_outward/gate_outward.js
+++ embroidery/embroidery/doctype/gate_outward/gate_outward.js
@@ -28,21 +28,22 @@ frappe.ui.form.on("Gate Outward Item", {
 function calculate_rate(frm, cdt, cdn) {
     let row = locals[cdt][cdn];
 
-        // Default to 0
-        let rate = 0;
+    let stitches = Number(row.stitches) || 0;
+    let constant = Number(row.constant) || 0;
+    let qty = Number(row.qty) || 0;
 
-        // Apply conditions
-        if (row.embroidery_type === "Alternate" || row.embroidery_type === "Folding") {
-            rate = (row.stitches / 1000) * row.constant;
-        } else if (row.embroidery_type === "Sheet") {
-            rate = ((row.stitches * 3) / 1000) * row.constant * 1;
-        } else {
-            rate = 0;
-        }
+    let rate = 0;
 
-        // Set value in child table row
-        frappe.model.set_value(cdt, cdn, "rate", rate);
-        frappe.model.set_value(cdt, cdn, "amount", row.rate * row.qty || 0);
+    if (row.embroidery_type === "Alternate" || row.embroidery_type === "Folding") {
+        rate = (stitches / 1000) * constant;
+    } else if (row.embroidery_type === "Sheet") {
+        rate = (stitches * 3 / 1000) * constant;
+    }
+
+    let amount = rate * qty;
+
+    frappe.model.set_value(cdt, cdn, {
+        rate: rate,
+        amount: amount
+    });
 }
-
-
