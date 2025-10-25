17f481d62e39aed22940d61dc2b698ef8a41f0ed
SafdarAliGit <safdar211@gmail.com>
2025-10-25 21:51:45 +0500

    updated

--- embroidery/embroidery/doctype/gate_pass/gate_pass.js
+++ embroidery/embroidery/doctype/gate_pass/gate_pass.js
@@ -10,6 +10,40 @@ frappe.ui.form.on("Gate Pass", {
                 }
             };
         });
-	},
+        frm.set_query("lot_no", "gate_pass_item", function(doc, cdt, cdn) {
+    // cdn is the name of the current child row (e.g., '1234567890')
+        const current_row = frappe.get_doc(cdt, cdn); 
+        
+        // Check if item_code is available in the current child row
+        if (current_row.item) {
+            return {
+                filters: {
+                    item: current_row.item
+                }
+            };
+        }
+        
+        // Return empty filters if no item_code is found, so it shows all lots or none
+        return { filters: {} };
+        });
+        },
 });
 
+
+
+frappe.ui.form.on("Gate Pass Item", {
+
+    qty: function(frm, cdt, cdn) {
+        total_qty(frm,);
+    }
+});
+	
+
+function total_qty(frm) {
+    let total = 0;
+    for (let i = 0; i < frm.doc.gate_pass_item.length; i++) {
+        total += frm.doc.gate_pass_item[i].qty;
+    }
+    frm.set_value("total_qty", total);
+}
+    
